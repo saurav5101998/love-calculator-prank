e66adc8c5a28669410e31ef7740153535b56253a
Saurav Kumar <63443496+saurav5101998@users.noreply.github.com>
2020-10-31 17:22:45 +0530

    create connection.php
    adding this for hacktoberfest

---
+++ includes/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbhost = "localhost";
+$dbusername = "id10141519_root";
+$dbpassword = "12345";
+$dbname = "id10141519_lovecalc";
+
+$conn = mysqli_connect($dbhost , $dbusername , $dbpassword , $dbname);
+
+
+?>
