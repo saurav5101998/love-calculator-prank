508685b76dfe811ac859532ef48f1165585bbc72
Saurav Kumar <63443496+saurav5101998@users.noreply.github.com>
2020-10-31 17:25:25 +0530

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="extras/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" crossorigin="anonymous">
+  
+  <link rel="stylesheet" type="text/css" href="./css/style.css">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <title>Love Calculator</title>
+</head>
+<body>
